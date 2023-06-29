4e6582546c16ed3af41f20ac8ab323f732b4295b
apple-x-co <apple.x.co@gmail.com>
2023-06-29 16:52:21 +0900

    add intervention-image sample

---
+++ intervention-image/index.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+require 'vendor/autoload.php';
+
+use Intervention\Image\AbstractFont;
+use Intervention\Image\ImageManager;
+
+$manager = new ImageManager(['driver' => 'gd']);
+
+$canvas = $manager->canvas(1200, 630);
+
+$canvas->insert(__DIR__ . '/../assets/bg.jpg', 'top-left');
+$canvas->insert(__DIR__ . '/../assets/logo.png', 'top-left');
+$canvas->text(
+    '1234567890',
+    1000,
+    40,
+    function(AbstractFont $font) {
+        $font->file(__DIR__ . '/../assets/m-plus-1-v6-japanese_latin-regular.ttf');
+        $font->size(26.67); // 20pt
+        $font->color('#90ee90');
+//        $font->align('center');
+//        $font->valign('top');
+//        $font->angle(45);
+    },
+);
+$canvas->text(
+    'Kouhei',
+    550,
+    330,
+    function(AbstractFont $font) {
+        $font->file(__DIR__ . '/../assets/m-plus-1-v6-japanese_latin-regular.ttf');
+        $font->size(40); // 30pt
+        $font->color('#000000');
+    },
+);
+
+$canvas->save(__DIR__ . '/./output/composited.png');
